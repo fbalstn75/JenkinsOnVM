12db7b5cee605743663d2fd72bade3037f16e2be
Minsu <ryualstn@naver.com>
2021-07-15 13:49:06 +0900

    10th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like UUU!');
+    res.send('I like UUUU!');
 });
 
 app.get('/start', function(req, res){
